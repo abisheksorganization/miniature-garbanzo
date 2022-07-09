3d4827863a02d65f0ec87794d11da220e4036c3a
Kirtana Kirtivasan <101681237+kkirtivasan@users.noreply.github.com>
2022-07-09 16:59:25 -0700

    Added a parameter for greeting message (#3)
    Added a parameter for greeting message

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -1,11 +1,14 @@
 package com.example;
 
 public class Main {
+
+    public static final String GREETING_MESSAGE = "Hello world!! How are you?";
+
     public static void main(String[] args) {
         greet();
     }
 
     public static void greet() {
-        System.out.println("Hello world!! How are you?");
+        System.out.println(GREETING_MESSAGE);
     }
 }
