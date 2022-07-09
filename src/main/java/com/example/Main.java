dfbd41652250442d6db5c91f4ab56b7356e44531
Abishek Ramasubramanian <abishek.ramasubraman@walmart.com>
2022-07-09 16:16:16 -0700

    Added a function to greet

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -2,6 +2,10 @@ package com.example;
 
 public class Main {
     public static void main(String[] args) {
+        greet();
+    }
+
+    public static void greet() {
         System.out.println("Hello world");
     }
 }
